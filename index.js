3c185a55336890ca3dea8c4d329bd0a5c4aec15a
Drxl95 <uebelhorr@gmail.com>
2021-08-11 11:59:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const cors = require('cors');
-// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
 
 
 const mongoose = require('mongoose');
@@ -32,19 +32,19 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//CORS in express
-// app.use(cors({
-//     origin: (origin, callback) => {
-//         if (!origin) return callback(null, true);
-//         if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-//             let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-//             return callback(new Error(message), false);
-//         }
-//         return callback(null, true);
-//     }
-// }));
+CORS in express
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+            let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
 
-app.use(cors());
+
 
 //import auth.js to index.js
 let auth = require('./auth')(app);
