cd360d357569a531e4c85e1c57ce31dea14ab05d
Drxl95 <uebelhorr@gmail.com>
2021-09-02 21:02:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Cors
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://myflix20.netlify.app', 'https://myflixdb20.herokuapp.com/login'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://myflix20.netlify.app', 'https://myflix20.netlify.app/register', 'https://myflixdb20.herokuapp.com/login'];
 
 
 const mongoose = require('mongoose');
