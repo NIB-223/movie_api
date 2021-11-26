1ac5fe54ee60755bdd597a681add583eb5ce4831
Robert <robertu.dev@gmail.com>
2021-11-26 12:03:49 -0600

    removed double-call of db

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const { check, validationResult } = require('express-validator');
 /* mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true}); */
 
 //connect database on the web
-mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const { response } = require('express');
 const express = require('express');
